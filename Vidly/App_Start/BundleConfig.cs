989b75be9cab8fc5c42de6fa05f962a76eff0399
Badri <badri.py@outlook.com>
2019-03-29 20:54:53 +0530

    Create the basic movie model, view and controller. New bootstrap css is added and bundle config and route configs updated

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Vidly
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-sketchy.css",
                       "~/Content/site.css"));
         }
     }
