4c768078bed996e3e502e2668f10db6c9f9fd379
Badri <badri.py@outlook.com>
2019-03-29 21:25:40 +0530

    remove the bootstrap css

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Vidly
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-sketchy.css",
+                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
