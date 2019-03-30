84a6f15d907af6e3907a0ec6a24599bd3ab20a89
Badri <badri.py@outlook.com>
2019-03-30 19:01:51 +0530

    Add birthdate to the customer model and updated view and controller to show it

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Web;
 
 namespace Vidly.Models
@@ -9,9 +10,11 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+        public DateTime? BirthDate { get; set; }
 
         [Required]
         public string Name { get; set; }
+
         public bool IsSubscribedToNewsLetter { get; set; }
         public MemberShipType MemberShipType { get; set; }
         public byte MemberShipTypeId { get; set; }
