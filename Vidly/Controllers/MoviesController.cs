4203b9d7133d6e30d3436313edaf187819d12a84
Badri <badri.py@outlook.com>
2019-03-30 16:09:58 +0530

    Added the customer controller along with the views. Updated the layout.cshtml

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -8,9 +8,25 @@ using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
-    public class MovieController : Controller
+    public class MoviesController : Controller
     {
-        // GET: Movie
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Inception" },
+                new Movie { Id = 2, Name = "Prestige" }
+            };
+        }
+
+        [Route("Movie/Random")]
         public ActionResult Random()
         {
             var viewModel = new RandomMovieViewModel
