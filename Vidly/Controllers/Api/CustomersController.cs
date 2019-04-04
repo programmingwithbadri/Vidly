9a7fa502da1c6d821de4464a452d63903648a759
Badri <badri.py@outlook.com>
2019-04-04 10:06:13 +0530

    Create the WebApi for customer controller

---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        // GET: api/Customers
+        //If you don't give HTTP GET, mvc will automatically search for Get Action(method)
+        //in the controller to process HTTP GET function
+        // [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            var customers = _context.Customers.ToList();
+            if (customers == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return customers;
+        }
+
+        // GET: api/Customers/1
+        public Customer Get(int id)
+        {
+            var customer = _context.Customers.FirstOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return customer;
+        }
+
+        // POST: api/Customers
+        public void Post(Customer Customer)
+        {
+        }
+
+        // PUT: api/Customers/5
+        public void Put(int id, [FromBody]Customer Customer)
+        {
+        }
+
+        // DELETE: api/Customers/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
