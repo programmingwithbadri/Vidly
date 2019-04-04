77591d032959d16a8eb425e30663fa81396689fb
Badri <badri.py@outlook.com>
2019-04-04 10:11:48 +0530

    Create DTO for customer

---
+++ Vidly/DTOs/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.DTOs
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        
+        public DateTime? BirthDate { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+       
+        public byte MemberShipTypeId { get; set; }
+    }
+}
