5dcd49b945ec22fc8ae82a37f2a16fc7bc988591
Badri <badri.py@outlook.com>
2019-04-04 20:40:23 +0530

    coomet to explain about Dto mapping to domain

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -9,12 +9,21 @@ namespace Vidly.App_Start
         public MappingProfile()
         {
             // Domain to Dto
+
+            // Basically when your API goes outbound to send stuff to people
+            // you'll always pass your Objects through their specific Dto.
+            // So it has to map Customer -> CustomerDto.
             Mapper.CreateMap<Customer, CustomerDto>();
-            Mapper.CreateMap<CustomerDto, Customer>();
             Mapper.CreateMap<Movie, MovieDto>();
 
 
             // Dto to Domain
+            // Turning this around, when your API gets stuff sent by people,
+            // all the data passes through the Dto first, and then to the Customer Object.
+            // Now if you would copy paste the .ForMember line below the outbound mapping
+            // "route" you'd say to your AutoMapper "Hey, don't worry about the id, don't map that."
+
+            //Now if you would perform a GET request with postman at / api / customers, you'd still get all the data. Just that every id is 0, because you told AutoMapper to not care about that.
             Mapper.CreateMap<CustomerDto, Customer>()
                 .ForMember(c => c.Id, opt => opt.Ignore());
 
