a77a52e0c8c004dbebbe4f348b3627dda7ac26fb
Badri <badri.py@outlook.com>
2019-04-04 20:28:36 +0530

    Create the auto mapper profile for mapping customer to customer dto

---
+++ Vidly/App_Start/CustomerMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Vidly.DTOs;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class CustomerMapper : Profile
+    {
+        public CustomerMapper()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
